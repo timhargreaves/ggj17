8cbeabfea5b323993d11d36c52e2ad1e6166ce31
Tim Hargreaves <timhargreaves@jag.net>
2017-01-21 14:07:41 -0800

    Fish now spawns in random locations. Known issues: will spawn on 'land', random range should be linked to screen display instead of hardcoded

--- kanagawa/fish.py
+++ kanagawa/fish.py
@@ -1,5 +1,6 @@
 import pygame
 import userevents
+import random
 
 class Fish:
     # Shared
@@ -24,7 +25,12 @@ class Fish:
             gameDisplay.blit(self.imageAsset, (self.posX,self.posY))
 
     def update(self):
-        if self.timeToLive > 0:
-            self.timeToLive -=60
+        #if self.timeToLive > 0:
+        self.timeToLive -=60
         if self.timeToLive < 0:
             self.alive = False
+        if self.timeToLive < -5000:
+            self.timeToLive = 5000
+            self.posX = random.randint(0,800)
+            self.posY = random.randint(0,600)
+            self.alive = True
