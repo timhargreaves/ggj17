ba040e78dadec6c6af4dc7519788222f561ae2c4
Tim Hargreaves <timhargreaves@jag.net>
2017-01-20 20:23:38 -0800

    Added boat assets and drawing statically to screen

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((800,600))
+display_width = 800
+display_height = 600
+
+gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Main Window')
 
 clock = pygame.time.Clock()
 
 crashed = False
 backgroundImage = pygame.image.load('assets/background.png')
+boatImage = pygame.image.load('assets/boat.png')
 
 def background(x,y):
     gameDisplay.blit(backgroundImage, (x,y))
 
-x = 0
-y = 0
+def boat(x,y):
+    gameDisplay.blit(boatImage, (x,y))
+
+boatx = (display_width * 0.5)
+boaty = (display_height * 0.5)
+
 
 while not crashed:
     for event in pygame.event.get():
@@ -23,7 +31,9 @@ while not crashed:
 
         print(event)
 
-    background(x,y)
+    background(0,0)
+
+    boat(boatx,boaty)
 
     pygame.display.update()
     clock.tick(60)
