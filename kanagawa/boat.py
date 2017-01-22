83177fdc13b000820c882b274728931933a48896
Tim Hargreaves <timhargreaves@jag.net>
2017-01-21 20:48:38 -0800

    Convert boat to pygame sprite

--- kanagawa/boat.py
+++ kanagawa/boat.py
@@ -2,16 +2,13 @@ import pygame
 import userevents
 import math
 
-class Boat:
-    # Shared
-    imageAsset = pygame.image.load('assets/boat.png')
-    imageWidth = 50
-    imageHeight = 100
+class Boat(pygame.sprite.Sprite):
 
-    maxSpeed = 4.5
-
-    # Unique
     def __init__(self):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+
+        # Geometry and Physics
         self.posX = 400
         self.posY = 200
         self.velocity = 0
@@ -19,9 +16,17 @@ class Boat:
         self.deltaY = 0
         self.rot = 0
         self.deltaRot = 0
+        self.maxSpeed = 4.5
+
+        # Graphics
+        self.image = pygame.image.load('assets/boat.png')
+        self.rect = self.image.get_rect()
+
+        # Collision
+        self.mask = pygame.mask.from_surface(self.image)
 
     def draw(self,gameDisplay):
-        gameDisplay.blit(pygame.transform.rotate(self.imageAsset,self.rot), (self.posX,self.posY))
+        gameDisplay.blit(pygame.transform.rotate(self.image,self.rot), (self.posX,self.posY))
 
     def getForwardUnitVector(self):
         radAngle = math.radians(self.rot)
@@ -31,7 +36,6 @@ class Boat:
         # Vector.Up
         referenceVector = pygame.math.Vector2(0, 1)
         return pygame.math.Vector2(referenceVector.x * cos - referenceVector.y * sin, referenceVector.x * cos + referenceVector.y * cos)
-
 
 
     def update(self,events,deltaTime,screenMaxX,screenMaxY):
@@ -75,9 +79,9 @@ class Boat:
             self.posX += self.deltaX
         if self.deltaY < 0 and self.posY + self.deltaY > 0:
             self.posY += self.deltaY
-        if self.deltaX > 0 and self.posX + self.imageWidth + self.deltaX < screenMaxX:
+        if self.deltaX > 0 and self.posX + self.image.get_width() + self.deltaX < screenMaxX:
             self.posX += self.deltaX
-        if self.deltaY > 0 and self.posY + self.imageHeight + self.deltaY < screenMaxY:
+        if self.deltaY > 0 and self.posY + self.image.get_height() + self.deltaY < screenMaxY:
             self.posY += self.deltaY
 
         # Rotation
@@ -91,3 +95,6 @@ class Boat:
                 self.rot -= 360
             if self.rot < 0:
                 self.rot += 360
+
+        # Update mask after all movement
+        self.mask = pygame.mask.from_surface(self.image)
